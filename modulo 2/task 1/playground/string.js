0bdf7ef51496e0dd21510ab6ed1001331d078ce7
meji32 <59580994+meji32@users.noreply.github.com>
2020-01-24 09:35:55 -0300

    1/24 empezando día

---
+++ modulo 2/task 1/playground/string.js
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+// Exercise 1: Write a JavaScript function that reverses a number. For example, if x = 32443 then the output should be 34423.
+
+function reverseNumber() {
+	let number=("4567")
+	console.log(number.split("").reverse("").join(""))
+	
+=======
+// Exercise 1: Write a JavaScript function that reverses a number. For example, if x = 32443 then the output should be 34423.
+
+function reverseNumber() {
+	let number=("4567")
+	console.log(number.split("").reverse("").join(""))
+	
+>>>>>>> 1db4ddd7eb6f45d22993e90d65d6831132fb8e7d
+}reverseNumber()
